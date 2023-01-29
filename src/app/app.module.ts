bd558f29bf1396b0f3865c26b5f6c52719f86191
filipjovicccc <filipjovicccc@gmail.com>
2023-01-29 22:00:24 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    AddTodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
